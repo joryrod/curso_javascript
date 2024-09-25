2377e797a53b5ae2e2ef217c21c38622348372bb
joryrod <joryrodriguez25@gmail.com>
2024-09-25 13:01:52 -0500

    feet: completando la clase sobre operadores aritmeticos y realizando ejercicios para convertir operadores lineales

---
+++ 2.Valores_Operadores/Operadores.js
@@ -0,0 +1,19 @@
+// Suma
+console.log("Suma: ",4+5)
+// Resta
+console.log("Resta :",9+5)
+// Multiplicacion
+console.log("Multiplicacion :",8*2)
+// Divicion
+console.log("Divicion :",10/2)
+// Modulo
+console.log("Modulo :",10%2)
+// Potencia
+console.log("Potencia :",4**2)+
+
+/* realizar la operacion de sumar 4 y 100 y luego multiplicar el resultado por 11 */
+// estamos haciendo uso de la agrupacion de operacion.
+// precedencia de operadores.
+/* establece el orden el que se debe ejecutar cada operador */
+// para evitar recordar la precedencia se recomienda hacer uso de la agrupacion de operaciones.
+console.log("resulrado: ",(4+100)*11)
---
+++ 2.Valores_Operadores/ejercicios.js
@@ -0,0 +1,17 @@
+// convertir expreciones aritmeticas 
+// exprecion algoritmica
+// exprecion lineal
+console.log((2+3)/(5*2))
+console.log(((10+3)/6)+4)
+console.log(5+(10/(3+2)))
+console.log(5+(10/2)+7)
+
+// ejercicios
+console.log(((6/2)+(8/2))/((10/2)-(4/2)))
+console.log(2+(2+(2+4)/(3+1))/(2+(4/2)))
+console.log(2+4+(6/2)+(2/(6-3))/(8/(2+2)))
+console.log((8/(3+1))/(8/(7-3)))
+console.log((8+(10/(8+10)+(10/2)))/(8+(10/(2+3))))
+console.log((8+10+2)/(10+(6/2)))
+console.log((8+10+(6/(2*2)))/(8+10*(10/2)))
+console.log((8+10/2+12)/8)
