2a049d511a75aacb43ab7bbca8d6efda49ee4108
joryrod <joryrodriguez25@gmail.com>
2024-10-25 09:44:23 -0500

    feet: binding y pila de llamadas
    viendo de que trata el binding y la pila de llamadas

--- 4.funciones/funciones.js
+++ 4.funciones/funciones.js
@@ -13,6 +13,9 @@
 // binding en js lo que hace es subir las funciones declarativas a la cabecera de mi archivo.
 console.log(despedida())
 
+// en el caso de funciones con valor o funciones flecha el binding solo eleva al principio del archivo por eso podemos ejecutar la funcion desde cualquier parte del archivo incluso antes de que sea creada
+ 
+
 let saludo=()=>{return "hola primo"}
 function despedida(){
     return "adios"
